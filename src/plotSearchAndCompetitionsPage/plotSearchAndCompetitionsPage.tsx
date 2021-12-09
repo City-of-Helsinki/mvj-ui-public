f3ffac4c76b4a913fc0497e87b1097fd45ba6a67
Tomi Mäkelä <tomi.makela@vincit.fi>
2021-12-09 11:24:24 +0200

    Filter out empty search types

--- src/plotSearchAndCompetitionsPage/plotSearchAndCompetitionsPage.tsx
+++ src/plotSearchAndCompetitionsPage/plotSearchAndCompetitionsPage.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { RootState } from '../root/rootReducer';
@@ -15,11 +15,15 @@ import {
 
 import { Language } from '../language/types';
 import translations from './translations';
+import { ApiAttributes } from '../api/types';
+import { PlotSearch, PlotSearchTarget } from '../plotSearch/types';
 
 interface State {
   currentLanguage: Language;
   isFetchingPlotSearches: boolean;
   isFetchingPlotSearchAttributes: boolean;
+  plotSearches: Array<PlotSearch>;
+  plotSearchAttributes: ApiAttributes;
 }
 
 interface Props {
@@ -28,7 +32,21 @@ interface Props {
   fetchPlotSearchAttributes: () => void;
   isFetchingPlotSearches: boolean;
   isFetchingPlotSearchAttributes: boolean;
+  plotSearches: Array<PlotSearch>;
+  plotSearchAttributes: ApiAttributes;
 }
+
+export type CategoryOptions = Array<{
+  id: number;
+  labelKey: string;
+  symbol: string;
+}>;
+export type CategoryVisibilities = Record<number, boolean>;
+
+export type SelectedTarget = {
+  target: PlotSearchTarget;
+  plotSearch: PlotSearch;
+} | null;
 
 const PlotSearchAndCompetitionsPage = (props: Props): JSX.Element => {
   const {
@@ -37,8 +55,14 @@ const PlotSearchAndCompetitionsPage = (props: Props): JSX.Element => {
     fetchPlotSearchAttributes,
     isFetchingPlotSearches,
     isFetchingPlotSearchAttributes,
+    plotSearchAttributes,
+    plotSearches,
   } = props;
   const [activeTab, setActiveTab] = useState<number>(0);
+  const [categoryVisibilities, setCategoryVisibilities] =
+    useState<CategoryVisibilities>({});
+  const [categoryOptions, setCategoryOptions] = useState<CategoryOptions>([]);
+  const [selectedTarget, setSelectedTarget] = useState<SelectedTarget>(null);
   const navigate = useNavigate();
   const { search } = useLocation();
   const tab = new URLSearchParams(search).get('tab');
@@ -48,6 +72,32 @@ const PlotSearchAndCompetitionsPage = (props: Props): JSX.Element => {
     fetchPlotSearches();
     fetchPlotSearchAttributes();
   }, []);
+
+  useEffect(() => {
+    let newOptions = [...(plotSearchAttributes?.type?.choices || [])];
+
+    if (newOptions) {
+      newOptions.sort((a, b) => (a.value > b.value ? 1 : -1));
+      newOptions = newOptions.filter((option) =>
+        plotSearches.some((plotSearch) => plotSearch.type?.id === option.value)
+      );
+
+      setCategoryOptions(
+        newOptions.map((option, index) => ({
+          id: Number(option.value),
+          labelKey: option.display_name,
+          symbol: String.fromCharCode('A'.charCodeAt(0) + index),
+        }))
+      );
+
+      setCategoryVisibilities(
+        newOptions.reduce((acc, next) => {
+          acc[Number(next.value)] = true;
+          return acc;
+        }, {} as CategoryVisibilities)
+      );
+    }
+  }, [plotSearchAttributes, plotSearches]);
 
   if (tabId != activeTab) {
     setActiveTab(tabId);
@@ -62,6 +112,20 @@ const PlotSearchAndCompetitionsPage = (props: Props): JSX.Element => {
       pathname: location.pathname,
       search: `?tab=${tabId}`,
     });
+  };
+
+  const onToggleCategoryVisibility = (
+    categoryId: number,
+    isVisible: boolean
+  ) => {
+    setCategoryVisibilities(
+      Object.keys(categoryVisibilities).reduce((acc, key) => {
+        const keyIdx = Number(key);
+        acc[keyIdx] =
+          keyIdx === categoryId ? isVisible : categoryVisibilities[keyIdx];
+        return acc;
+      }, {} as CategoryVisibilities)
+    );
   };
 
   if (isFetchingPlotSearches || isFetchingPlotSearchAttributes) {
@@ -100,6 +164,8 @@ const PlotSearchAndCompetitionsPage = (props: Props): JSX.Element => {
 
 const mapStateToProps = (state: RootState): State => ({
   currentLanguage: state.language.current,
+  plotSearches: state.plotSearch.plotSearches,
+  plotSearchAttributes: state.plotSearch.plotSearchAttributes,
   isFetchingPlotSearches: state.plotSearch.isFetchingPlotSearches,
   isFetchingPlotSearchAttributes:
     state.plotSearch.isFetchingPlotSearchAttributes,
