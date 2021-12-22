1fe7563f3df6af1aa02451ad0710226273631144
Tomi Mäkelä <tomi.makela@vincit.fi>
2021-12-22 15:05:13 +0200

    Remove project boilerplate remnants

--- src/root/rootSaga.ts
+++ src/root/rootSaga.ts
@@ -1,8 +1,7 @@
 import { all, fork } from 'redux-saga/effects';
 import authSaga from '../auth/saga';
-import counterSaga from '../counter/saga';
 import plotSearchSaga from '../plotSearch/saga';
 
 export default function* rootSaga(): Generator {
-  yield all([fork(authSaga), fork(counterSaga), fork(plotSearchSaga)]);
+  yield all([fork(authSaga), fork(plotSearchSaga)]);
 }
