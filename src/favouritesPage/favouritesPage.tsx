acba8d6b38af81c4e69986d2cc9aad2243567a52
Tomi Mäkelä <tomi.makela@vincit.fi>
2022-03-03 17:55:52 +0200

    Only show the loader on favourite page until data has been fetched

--- src/favouritesPage/favouritesPage.tsx
+++ src/favouritesPage/favouritesPage.tsx
@@ -85,61 +85,71 @@ const FavouritesPage = (props: Props): JSX.Element => {
           )}
         </Col>
       </Row>
-      <Row>
-        {props.isFetchingPlotSearches ? (
-          <BlockLoader />
-        ) : (
-          props.favourite.targets.map((target) => (
-            <Col xs={12} key={target.plot_search_target.id}>
-              <FavouriteCard
-                plotSearch={plotSearch}
-                target={target.plot_search_target}
-                remove={handleTargetRemove}
-              />
-            </Col>
-          ))
-        )}
-      </Row>
-      <Row>
-        <Col md={6} xs={12} className="FavouritesPage__notification-container">
-          <Notification
-            type="info"
-            position="inline"
-            label={t(
-              'favouritesPage.notification.title',
-              'Do you want to search for more targets?'
-            )}
-          >
-            <Trans i18nKey="favouritesPage.notification.body">
-              <p>
-                You can find all the selected targets here. You can return to
-                plot search page to add more targets.
-              </p>
-            </Trans>
-            <p>
-              <Link
-                href={getRouteById(AppRoutes.PLOT_SEARCH_AND_COMPETITIONS)}
-                size="M"
-              >
-                {t(
-                  'favouritesPage.notification.link',
-                  'Return to plot search page'
+      {props.isFetchingPlotSearches ? (
+        <Row>
+          <Col xs={12}>
+            <BlockLoader />
+          </Col>
+        </Row>
+      ) : (
+        <>
+          <Row>
+            {props.favourite.targets.map((target) => (
+              <Col xs={12} key={target.plot_search_target.id}>
+                <FavouriteCard
+                  plotSearch={plotSearch}
+                  target={target.plot_search_target}
+                  remove={handleTargetRemove}
+                />
+              </Col>
+            ))}
+          </Row>
+          <Row>
+            <Col
+              md={6}
+              xs={12}
+              className="FavouritesPage__notification-container"
+            >
+              <Notification
+                type="info"
+                position="inline"
+                label={t(
+                  'favouritesPage.notification.title',
+                  'Do you want to search for more targets?'
                 )}
-              </Link>
-            </p>
-          </Notification>
-        </Col>
-      </Row>
-      <Row className="FavouritesPage__actions">
-        <Col xs={12}>
-          <Button
-            onClick={() => navigateToApplication()}
-            disabled={props.isLoadingUser}
-          >
-            {t('favouritesPage.nextButton', 'Apply for these plots')}
-          </Button>
-        </Col>
-      </Row>
+              >
+                <Trans i18nKey="favouritesPage.notification.body">
+                  <p>
+                    You can find all the selected targets here. You can return
+                    to plot search page to add more targets.
+                  </p>
+                </Trans>
+                <p>
+                  <Link
+                    href={getRouteById(AppRoutes.PLOT_SEARCH_AND_COMPETITIONS)}
+                    size="M"
+                  >
+                    {t(
+                      'favouritesPage.notification.link',
+                      'Return to plot search page'
+                    )}
+                  </Link>
+                </p>
+              </Notification>
+            </Col>
+          </Row>
+          <Row className="FavouritesPage__actions">
+            <Col xs={12}>
+              <Button
+                onClick={() => navigateToApplication()}
+                disabled={props.isLoadingUser}
+              >
+                {t('favouritesPage.nextButton', 'Apply for these plots')}
+              </Button>
+            </Col>
+          </Row>
+        </>
+      )}
     </Container>
   );
 };
