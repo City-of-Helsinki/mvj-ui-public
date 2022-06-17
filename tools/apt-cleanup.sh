a23f7fe94af30b21a5e048f779e3ad110571be88
Niko Vuorinen <niko.vuorinen@vincit.fi>
2022-06-17 15:37:02 +0300

    Change docker-compose configurations

---
+++ tools/apt-cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ ! -z "$@" ]]; then
+    apt-get remove -y "$@"
+fi
+
+apt-get purge -y --auto-remove -o APT::AutoRemove::RecommendsImportant=false
+rm -rf /var/lib/apt/lists/*
+rm -rf /var/cache/apt/archives
