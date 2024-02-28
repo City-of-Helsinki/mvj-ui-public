b12d87d5f7560510d689d18c36f7f364ab31a47f
Henri Nieminen <henri.nieminen@nordcloud.com>
2024-02-28 16:22:59 +0200

    Reintroduce hasApiToken to indicate user is logged in
    This cause an issue in Area Search once we changed intended_uses to require authentication. It was trying to load that before it had apiToken and that failed.

--- src/auth/components/authDependentContent.tsx
+++ src/auth/components/authDependentContent.tsx
@@ -2,12 +2,18 @@ import { connect } from 'react-redux';
 import type { User } from 'oidc-client-ts';
 
 import { RootState } from '../../root/rootReducer';
-import { getIsFetchingApiToken, getIsLoadingUser, getUser } from '../selectors';
+import {
+  getIsFetchingApiToken,
+  getIsLoadingUser,
+  getUser,
+  hasApiToken,
+} from '../selectors';
 
 interface Props {
   isLoadingUser: boolean;
   isLoadingApiToken: boolean;
   user: User | null;
+  hasApiToken: boolean;
   children: (loading: boolean, loggedIn: boolean) => JSX.Element | null;
 }
 
@@ -15,12 +21,14 @@ const AuthDependentContent = ({
   isLoadingUser,
   isLoadingApiToken,
   user,
+  hasApiToken,
   children,
 }: Props): JSX.Element | null =>
-  children(isLoadingUser || isLoadingApiToken, !!user);
+  children(isLoadingUser || isLoadingApiToken, !!user && hasApiToken);
 
 export default connect((state: RootState) => ({
   user: getUser(state),
   isLoadingUser: getIsLoadingUser(state),
   isLoadingApiToken: getIsFetchingApiToken(state),
+  hasApiToken: hasApiToken(state),
 }))(AuthDependentContent);
