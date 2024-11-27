9625dd81081c4cbe4748697af1c4fef3dcca391f
Jukka Ahonen <jukka.ahonen@nordcloud.com>
2024-11-27 14:03:30 +0200

    playwright: test Finnish language change

--- e2e/frontpage.spec.ts
+++ e2e/frontpage.spec.ts
@@ -6,3 +6,18 @@ test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle('City of Helsinki plot and land renting system');
 });
+
+test('changes into Finnish', async ({ page }) => {
+  await page.goto('/');
+
+  const languageButton = page.getByLabel('Language');
+
+  languageButton.click();
+  
+  const finnishButton = languageButton.getByText('Suomi');
+  
+  finnishButton.click();
+  
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle('City of Helsinki plot and land renting system');
+});
