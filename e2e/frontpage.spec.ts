aa97bc5d794c5a6dfc7773f8011a25b1a8ce25b8
Henri Nieminen <henri.nieminen@nordcloud.com>
2024-11-27 15:42:53 +0200

    add e2e tests for different language titles

--- e2e/frontpage.spec.ts
+++ e2e/frontpage.spec.ts
@@ -1,23 +1,34 @@
 import { test, expect } from '@playwright/test';
+import finnishTranslations from '../src/i18n/fi/common.json' with { type: "json" };
+import englishTranslations from '../src/i18n/en/common.json' with { type: "json" };
+import swedishTranslations from '../src/i18n/sv/common.json' with { type: "json" };
 
-test('has title', async ({ page }) => {
+
+
+test('has correct title in Finnish', async ({ page }) => {
   await page.goto('/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle('City of Helsinki plot and land renting system');
+  const finnishTitle = finnishTranslations['mainAppTitle'];
+  await expect(page).toHaveTitle(finnishTitle);
 });
 
-test('changes into Finnish', async ({ page }) => {
+test('has correct title in English', async ({ browser }) => {
+  const context = await browser.newContext({locale: 'en'});
+  const page = await context.newPage();
   await page.goto('/');
-
-  const languageButton = page.getByLabel('Language');
-
-  languageButton.click();
-  
-  const finnishButton = languageButton.getByText('Suomi');
-  
-  finnishButton.click();
-  
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle('City of Helsinki plot and land renting system');
+  await expect(page).toHaveTitle(englishTranslations['mainAppTitle']);
 });
+
+test('has correct title in Swedish', async ({ browser }) => {
+  const context = await browser.newContext({locale: 'sv'});
+  const page = await context.newPage();
+  await page.goto('/');
+  await expect(page).toHaveTitle(swedishTranslations['mainAppTitle']);
+
+});
+
+test('Accepting cookies hides cookie banner', async ({ page }) => {
+  await page.goto('/');
+  const cookieApproveButton = page.getByTestId('cookie-consent-approve-button');
+  await cookieApproveButton.click();    
+  await expect(cookieApproveButton).toHaveCount(0);
+  });
