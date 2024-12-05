d240a68808f8894e386d7ea2ccb720be6180da27
Juho Kettunen <juho.kettunen@nordcloud.com>
2024-12-05 15:46:03 +0200

    Use user and group ID 5000
    This is best practice recommended by Red Hat.

--- scripts/setup_user.sh
+++ scripts/setup_user.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-existing_group=$(getent group 1000 | cut -d: -f1)
+existing_group=$(getent group 5000 | cut -d: -f1)
 if [[ -z "${existing_group}" ]]; then
     # Create appuser group
-   groupadd --gid 1000 appuser
+   groupadd --gid 5000 appuser
 elif [[ "${existing_group}" != "appuser" ]]; then
    # Change group name to appuser
    groupmod -n appuser "${existing_group}"
 fi
 
 
-existing_user=$(id -nu 1000 2>/dev/null)
+existing_user=$(id -nu 5000 2>/dev/null)
 if [[ -z "${existing_user}" ]]; then
     # Add an application user
-    useradd --uid 1000 --gid appuser --create-home --shell /bin/bash appuser
+    useradd --uid 5000 --gid appuser --create-home --shell /bin/bash appuser
 else
     # Change user name to appuser
     usermod -l appuser "${existing_user}"
