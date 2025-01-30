cf5092c1cb0465a2bd43fbf4aad089f1ddb1583a
Henri Nieminen <henri.nieminen@nordcloud.com>
2025-01-30 15:30:52 +0200

    add feature flags

---
+++ src/featureFlags.ts
@@ -0,0 +1,4 @@
+export const IS_FEATURE_PLOT_SEARCH_ENABLED =
+  import.meta.env.VITE_FLAG_PLOT_SEARCH_ENABLED === 'true';
+export const IS_FEATURE_OTHER_SEARCH_ENABLED =
+  import.meta.env.VITE_FLAG_OTHER_SEARCH_ENABLED === 'true';
