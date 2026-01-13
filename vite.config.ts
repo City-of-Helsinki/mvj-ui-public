46257ac3e8e54af8ef09d9a408afa2525908fcbd
Henri Nieminen <henri.nieminen@nordcloud.com>
2026-01-13 13:48:35 +0000

    add sonarqube cloud scan

--- vite.config.ts
+++ vite.config.ts
@@ -6,24 +6,32 @@ import { viteStaticCopy } from 'vite-plugin-static-copy';
 import postcssUrl from 'postcss-url';
 // https://vitejs.dev/config/
 
-
 export default defineConfig({
   base: '/',
   build: {
-    outDir: 'build'
+    outDir: 'build',
   },
   test: {
-    include: ['**/*{test,spec}.?(c|m)[jt]s?(x)',],
+    include: ['src/**/*{test,spec}.{ts,tsx}'],
+    exclude: ['e2e/**'],
     globals: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['lcov', 'text'],
+      reportsDirectory: './coverage',
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: ['src/**/*.{test,spec}.{ts,tsx}', 'src/**/*.d.ts'],
+    },
   },
   plugins: [
     viteStaticCopy({
       targets: [
-        { // copy 3rd party assets to build
+        {
+          // copy 3rd party assets to build
           src: 'node_modules/leaflet-draw/dist/images/*',
-          dest: 'assets/images/.'
+          dest: 'assets/images/.',
         },
-      ]
+      ],
     }),
     react(),
     svgr({
@@ -35,19 +43,22 @@ export default defineConfig({
       plugins: [
         postcssUrl({
           url: (asset) => {
-            if (asset.relativePath && !['src', 'assets'].includes(asset.relativePath.split('/')[0])) {
+            if (
+              asset.relativePath &&
+              !['src', 'assets'].includes(asset.relativePath.split('/')[0])
+            ) {
               return `/assets/${asset.url}`;
             }
             return asset.url;
-          }
+          },
         }),
-      ]
-    }
+      ],
+    },
   },
   envDir: './',
   server: {
     host: '0.0.0.0',
     port: 4000,
-    origin: 'http://localhost:4000'
-  }
+    origin: 'http://localhost:4000',
+  },
 });
